cd8c0136f3cf2ad11b8a621e36cca04fa286380a
Arkarivs <adrianmorangalan@gmail.com>
2025-10-16 20:31:59 +0200

    FIX: stylize & refactor
    - mover scripts y estilos a archivos dedicados
    - enlazar los scripts y estilos en el index
    - reducir numero de entradas del blog renderizados en el index
    - estilar la pagina/blog
    
    - TO-DO: pagina dedicada de entradas al blog
    
    Co-Authored-By: Ami <143962821+sleepingami@users.noreply.github.com>

---
+++ scripts/script.js
@@ -0,0 +1,40 @@
+const repost = document.getElementById("ark");
+async function publicaciones() {
+    try {
+        const respuesta = await fetch(`https://arkarivs.github.io/mi-blog/mango/index.json`, {
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+        if(!respuesta.ok) {
+            throw new Error('HTTP Error ' + respuesta.status);
+        }
+        return await respuesta.json();
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
+
+publicaciones().then(data => {
+    if(!data || !Array.isArray(data) || data.length === 0) {
+        repost.innerHTML = "<p>no post</p>";
+        return;
+    }
+
+    const limit = data.slice(0, 3);
+
+    repost.innerHTML = limit.map(post => {
+        const url = post.url || ('/mango/' + post.id || '');
+        return `
+        <br>
+        <a href="/mi-blog${url}" style="color: white;">
+            <div id="miid" class="miclass" style="border: 2px solid green; border-radius: 13px;">
+                <div style="position: relative;">
+                    <p style="cursor: default; postion: relative; left: 12px; padding: 8px;">${post.title}</p>
+                </div>    
+            </div>
+        </a>
+        `;
+    })
+})
